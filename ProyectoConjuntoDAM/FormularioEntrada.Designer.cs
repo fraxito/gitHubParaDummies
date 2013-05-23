0a1ef9ddbd1a4f20b1995e7d92a80e4d1747327e
Raul Sanchez Mondejar <rulaspirulas@yahoo.es>
2013-05-23 11:42:25 +0200

    2013.05.23 v1
    se me enamora el almaaaaaa

--- ProyectoConjuntoDAM/FormularioEntrada.Designer.cs
+++ ProyectoConjuntoDAM/FormularioEntrada.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.botonJorgeCisneros = new System.Windows.Forms.Button();
+            this.button_De_La_Muerte = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -50,11 +51,22 @@
             this.botonJorgeCisneros.Text = "JorgeCisneros";
             this.botonJorgeCisneros.UseVisualStyleBackColor = true;
             // 
+            // button_De_La_Muerte
+            // 
+            this.button_De_La_Muerte.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_De_La_Muerte.Location = new System.Drawing.Point(89, 235);
+            this.button_De_La_Muerte.Name = "button_De_La_Muerte";
+            this.button_De_La_Muerte.Size = new System.Drawing.Size(405, 178);
+            this.button_De_La_Muerte.TabIndex = 2;
+            this.button_De_La_Muerte.Text = "Botonaco";
+            this.button_De_La_Muerte.UseVisualStyleBackColor = true;
+            // 
             // FormularioEntrada
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 484);
+            this.Controls.Add(this.button_De_La_Muerte);
             this.Controls.Add(this.botonJorgeCisneros);
             this.Controls.Add(this.label1);
             this.Name = "FormularioEntrada";
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button botonJorgeCisneros;
+        private System.Windows.Forms.Button button_De_La_Muerte;
     }
 }
 
