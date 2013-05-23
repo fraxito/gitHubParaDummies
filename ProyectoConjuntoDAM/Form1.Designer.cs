27d84929970256b2af607afa731e2f9b1a58af93
Javi <javi.th1@gmail.com>
2013-05-23 13:01:13 +0200

    Boton y Form creado

---
+++ ProyectoConjuntoDAM/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ProyectoConjuntoDAM
+{
+    partial class JaviGV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonJaviGV = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonJaviGV
+            // 
+            this.buttonJaviGV.Location = new System.Drawing.Point(75, 69);
+            this.buttonJaviGV.Name = "buttonJaviGV";
+            this.buttonJaviGV.Size = new System.Drawing.Size(75, 23);
+            this.buttonJaviGV.TabIndex = 0;
+            this.buttonJaviGV.Text = "JaviGV";
+            this.buttonJaviGV.UseVisualStyleBackColor = true;
+            // 
+            // JaviGV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonJaviGV);
+            this.Name = "JaviGV";
+            this.Text = "JaviGV";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonJaviGV;
+    }
+}
